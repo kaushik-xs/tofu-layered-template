26a342fd8e7856daccaa04fd71b483fa6caf2649
Kaushik Sundar <57823802+kaushik-xs@users.noreply.github.com>
2026-04-21 14:43:03 +0530

    Refactor output definitions for global identity layer
    - Moved output definitions for `layer_name`, `workspace`, and `route53_hosted_zone_ids` from `main.tf` to `outputs_context.tf` for better organization and clarity.
    - This change enhances the structure of the Terraform configuration by centralizing output management.

--- layers/global_identity/outputs_context.tf
+++ layers/global_identity/outputs_context.tf
@@ -12,3 +12,16 @@ output "gcp_region" {
   description = "GCP region configured for this layer"
   value       = var.gcp_region
 }
+
+output "layer_name" {
+  value = "global_identity"
+}
+
+output "workspace" {
+  value = local.workspace
+}
+
+output "route53_hosted_zone_ids" {
+  description = "Map of hosted zone DNS name (each.key) to Route53 zone ID for aws_route53_zone.primary."
+  value       = { for zone_name, z in aws_route53_zone.primary : zone_name => z.zone_id }
+}
